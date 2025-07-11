e4c79ecf1f55b392a9ea20f6376928f04e05f91f
Lian4445437 <liancarmodasilva882@gmail.com>
2025-07-10 21:09:15 -0300

    Add files via upload

---
+++ index.js.js
@@ -0,0 +1,9 @@
+var botão = document.querySelector('#botão');
+var resultado = document.querySelector('#resultado')
+
+botão.addEventListener('click',abertar);
+
+function abertar(){
+    resultado.innerHTML = 'OBRIGADO POR LER ATÉ AQUI'
+
+}
